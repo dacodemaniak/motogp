3a11ca92f7ef9a8fc27afc2d19a8d4d4b90d55d5
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-07-21 16:46:32 +0200

    Add sorter

--- table.js
+++ table.js
@@ -21,61 +21,101 @@ console.log('Hello JS')
 const interns = [
     {
         id: 1,
-        lastname: 'Aubert',
+        lastname: 'Zanzibar',
         firstname: 'Jean-Luc',
         phone: '06 23 23 23 23',
         mail: 'jean-luc.aubert@aelion.fr'
     },
     {
         id: 2,
-        lastname: 'Aubert',
+        lastname: 'Dutronc',
         firstname: 'Jean-Luc',
         phone: '06 23 23 23 23',
         mail: 'jean-luc.aubert@aelion.fr'
     },
     {
         id: 3,
-        lastname: 'Aubert',
+        lastname: 'Tartuffe',
         firstname: 'Jean-Luc',
         phone: '06 23 23 23 23',
         mail: 'jean-luc.aubert@aelion.fr'
+    },
+    {
+        id: 4,
+        lastname: 'Bond',
+        firstname: 'James',
+        phone: '07 07 07 07 07',
+        mail: 'bond.james'
     }
 ]
 
 // Displaying all of interns in a row in a table
-const tbody = document.querySelector('tbody')
+const rowMaker = () => {
 
-for (const intern of interns) {
-    // 1. Create a tr element
-    const newTr = document.createElement('tr')
 
-    // 2. Create a td element and write intern id into
-    const idTd = document.createElement('td')
-    idTd.innerHTML = intern.id
-    // 3. Add the brand new td to tr
-    newTr.appendChild(idTd)
+    const tbody = document.querySelector('tbody')
 
-    // 2. Create a td element and write intern lastname into
-    const lastnameTd = document.createElement('td')
-    lastnameTd.innerHTML = intern.lastname
-    // 3. Add the brand new td to tr
-    newTr.appendChild(lastnameTd)
+    for (const intern of interns) {
+        // 1. Create a tr element
+        const newTr = document.createElement('tr')
 
-    // 2. Create a td element and write intern firstname into
-    const firstnameTd = document.createElement('td')
-    firstnameTd.innerHTML = intern.firstname
-    // 3. Add the brand new td to tr
-    newTr.appendChild(firstnameTd)
+        // 2. Create a td element and write intern id into
+        const idTd = document.createElement('td')
+        idTd.innerHTML = intern.id
+        // 3. Add the brand new td to tr
+        newTr.appendChild(idTd)
 
-    const phoneTd = document.createElement('td')
-    phoneTd.innerHTML = intern.phone
-    newTr.appendChild(phoneTd)
-    
-    const emailTd = document.createElement('td')
-    emailTd.innerHTML = intern.mail
-    newTr.appendChild(emailTd)
+        // 2. Create a td element and write intern lastname into
+        const lastnameTd = document.createElement('td')
+        lastnameTd.innerHTML = intern.lastname
+        // 3. Add the brand new td to tr
+        newTr.appendChild(lastnameTd)
 
-    // Append the brand new complete tr to tbody (that's all folks)
-    tbody.appendChild(newTr)
+        // 2. Create a td element and write intern firstname into
+        const firstnameTd = document.createElement('td')
+        firstnameTd.innerHTML = intern.firstname
+        // 3. Add the brand new td to tr
+        newTr.appendChild(firstnameTd)
+
+        const phoneTd = document.createElement('td')
+        phoneTd.innerHTML = intern.phone
+        newTr.appendChild(phoneTd)
+        
+        const emailTd = document.createElement('td')
+        emailTd.innerHTML = intern.mail
+        newTr.appendChild(emailTd)
+
+        // Append the brand new complete tr to tbody (that's all folks)
+        tbody.appendChild(newTr)
+    }
+}
+
+rowMaker()
+
+// Arrow function
+const sortTableOnName = () => {
+    const trs = [... document.querySelector('tbody').childNodes]
+    for(let tr of trs) {
+        tr.remove()
+    }
+
+    interns.sort((intern1, intern2) => {
+        return intern1.lastname.localeCompare(intern2.lastname)
+    })
+    console.log(interns)
+    rowMaker()
+}
+
+function sortTable() {
+    const trs = [... document.querySelector('tbody').childNodes]
+    for(let tr of trs) {
+        tr.remove()
+    }
+
+    interns.sort((intern1, intern2) => {
+        return intern1.lastname.localeCompare(intern2.lastname)
+    })
+    console.log(interns)
+    rowMaker()
 }
 
