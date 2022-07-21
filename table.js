bd7308eff148109dde5258e14e3400844590f9d1
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-07-21 14:49:51 +0200

    Procedural mode

--- table.js
+++ table.js
@@ -47,15 +47,33 @@ const tbody = document.querySelector('tbody')
 
 for (const intern of interns) {
     // 1. Create a tr element
-    const newTr = htmlBuilder('tr')
+    const newTr = document.createElement('tr')
 
-    // 2. Create a td element and write intern infos into
-    const idTd = htmlBuilder('td', newTr, intern.id)
-    const lastnameTd = htmlBuilder('td', newTr, intern.lastname)
-    const firstnameTd = htmlBuilder('td', newTr, intern.firstname)
-    const phoneTd = htmlBuilder('td', newTr, intern.phone)
-    const emailTd = htmlBuilder('td', newTr, intern.mail)
+    // 2. Create a td element and write intern id into
+    const idTd = document.createElement('td')
+    idTd.innerHTML = intern.id
+    // 3. Add the brand new td to tr
+    newTr.appendChild(idTd)
 
+    // 2. Create a td element and write intern lastname into
+    const lastnameTd = document.createElement('td')
+    lastnameTd.innerHTML = intern.lastname
+    // 3. Add the brand new td to tr
+    newTr.appendChild(lastnameTd)
+
+    // 2. Create a td element and write intern firstname into
+    const firstnameTd = document.createElement('td')
+    firstnameTd.innerHTML = intern.firstname
+    // 3. Add the brand new td to tr
+    newTr.appendChild(firstnameTd)
+
+    const phoneTd = document.createElement('td')
+    phoneTd.innerHTML = intern.phone
+    newTr.appendChild(phoneTd)
+    
+    const emailTd = document.createElement('td')
+    emailTd.innerHTML = intern.mail
+    newTr.appendChild(emailTd)
 
     // Append the brand new complete tr to tbody (that's all folks)
     tbody.appendChild(newTr)
